9b66da5a459e70d519b3df2faf58eaeb196d5c7a
Eduardo Nogueira <132234306+Nogueira237@users.noreply.github.com>
2025-09-21 15:11:32 -0300

    Exercício 03 C++ [não funciona em versões mais antigas de c++]

---
+++ Exercício 06, C++ - Classes professor e aluno.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <locale.h>
+#include <list>
+using namespace std;
+
+
+//classe base
+class Pessoa{
+public:
+	string nome;
+
+	//construtor
+	Pessoa(string nome){
+		this->nome = nome;
+	}
+	//metodos
+	virtual void apresentar(){		// vitual para ser sobrescrito nos filhos
+		cout << "Olá, meu nome é " << nome << " e eu sou uma pessoa" << endl;
+	}
+};
+
+class Professor : public Pessoa{
+public: 
+	string disciplina;
+	
+	//construtor
+	Professor(string nome, string disciplina) : Pessoa(nome){
+		this->disciplina = disciplina;
+	}
+	
+	//metodos
+	void apresentar() override {
+		cout << "Olá, meu nome é " << nome << " e eu sou um professor de " << disciplina << endl;
+	}
+};
+
+class Aluno : public Pessoa{
+public: 
+	string curso;
+	int matricula;
+	
+	//construtor
+	Aluno(string nome,string curso, int matricula) : Pessoa(nome){
+		this->curso = curso;
+		this->matricula = matricula;
+	}
+	
+	//metodos
+	void apresentar() override {
+		cout << "Ola, meu nome é " << nome << " e eu sou um aluno de " << curso << endl;
+	}
+	
+};
+
+int main(){
+	setlocale( LC_ALL, "Portuguese" ); // permite acentos no console
+	
+	Pessoa* prof1 = new Professor("Marcelo", "S01");
+	Pessoa* alun1 = new Aluno("Eduardo", "S01", 597);
+	
+	list<Pessoa*> pessoas;
+	pessoas.push_back(prof1);
+	pessoas.push_back(alun1);
+	
+	for(Pessoa* it : pessoas){
+		it->apresentar();
+	};
+	
+	delete prof1;
+	delete alun1;
+	
+	return 0;
+};
+
